cb3bb6f005f19e4107bc25ab712ef8e7abcf61e5
EntryPoint <entrypointkr@gmail.com>
2021-10-07 07:43:42 +0900

    chore: prepare for publish to maven central

--- buildSrc/src/main/kotlin/spigradle-annotations-publish.gradle.kts
+++ buildSrc/src/main/kotlin/spigradle-annotations-publish.gradle.kts
@@ -1,9 +1,8 @@
 import kr.entree.spigradle.annotations.build.VersionTask
-import java.util.*
 
 plugins {
-    id("com.jfrog.bintray")
     id("maven-publish")
+    id("signing")
 }
 
 publishing {
@@ -14,28 +13,8 @@ publishing {
     }
 }
 
-bintray {
-    user = findProperty("bintray.publish.user")?.toString()
-    key = findProperty("bintray.publish.key")?.toString()
-    setPublications("spigradleAnnotations")
-    publish = true
-    pkg.apply {
-        repo = "Spigradle"
-        name = project.name
-        desc = project.description
-        websiteUrl = "https://github.com/spigradle/spigradle-annotations"
-        githubRepo = "https://github.com/spigradle/spigradle-annotations"
-        issueTrackerUrl = "https://github.com/spigradle/spigradle-annotations/issues"
-        setLicenses("Apache-2.0")
-        vcsUrl = "https://github.com/spigradle/spigradle-annotations.git"
-    }
-    project.afterEvaluate {
-        pkg.version.apply {
-            name = project.version.toString()
-            released = Date().toString()
-            vcsTag = project.version.toString()
-        }
-    }
+signing {
+    sign(publishing.publications["spigradleAnnotations"])
 }
 
 tasks.register("setVersion", VersionTask::class.java)
