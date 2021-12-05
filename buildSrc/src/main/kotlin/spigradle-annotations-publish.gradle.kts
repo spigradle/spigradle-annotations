f608dde7e45359272a1bfbb23fc04720ad0c0cbd
EntryPoint <entrypointkr@gmail.com>
2021-12-06 04:02:19 +0900

    chore(publish): add some metadata

--- buildSrc/src/main/kotlin/spigradle-annotations-publish.gradle.kts
+++ buildSrc/src/main/kotlin/spigradle-annotations-publish.gradle.kts
@@ -9,6 +9,30 @@ publishing {
     publications {
         create("spigradleAnnotations", MavenPublication::class) {
             from(components["java"])
+            // NOTE: https://central.sonatype.org/publish/requirements/#sufficient-metadata
+            pom {
+                name.set("${project.group}:${project.name}")
+                description.set("Annotations and processors for specify the main class.")
+                url.set("https://github.com/spigradle/spigradle-annotations")
+                licenses {
+                    license {
+                        name.set("The Apache License, Version 2.0")
+                        url.set("http://www.apache.org/licenses/LICENSE-2.0.txt")
+                    }
+                }
+                developers {
+                    developer {
+                        id.set("entrypointkr")
+                        name.set("Junhyung Im")
+                        email.set("entrypointkr@gmail.com")
+                    }
+                }
+                scm {
+                    connection.set("scm:git:git://github.com/spigradle/spigradle-annotations.git")
+                    developerConnection.set("scm:git:ssh://github.com:spigradle/spigradle-annotations.git")
+                    url.set("https://github.com/spigradle/spigradle-annotations/tree/master")
+                }
+            }
         }
     }
     repositories {
